45df75890890c24c3a1c369fc47f0a3a4abe2eb8
mem802 <44987685+mem802@users.noreply.github.com>
2018-12-09 19:43:37 -0500

    Delete else-if-statement.js

--- else-if-statement.js
+++
@@ -1,8 +0,0 @@
-var m=-10
-if (m=10){
-	console.log("The value of m is equal to 10");
-} else if(m<0 && m%2=0){
-	console.log("The value of m is less than zero and even");
-} else {
-	console.log("The value is something else");
-}
